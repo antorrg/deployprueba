023301466b15424e26a521ba0aafcd1c402505ae
antorrg <114789901+antorrg@users.noreply.github.com>
2024-04-14 01:39:05 -0300

    commit reparacion base de datos (conexion)

--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,16 @@ import dotenv from "dotenv";
 dotenv.config();
 const { DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,
-  { logging: false, native: false }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,
+//   { logging: false, native: false }
+// );
 
+//? Configuracion Raillway:
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+ });
 // const sequelize = new Sequelize(DB_DEPLOY, {
 //     logging: false, // set to console.log to see the raw SQL queries
 //      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
